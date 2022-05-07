dc79a084d96b64e8d9f3ba3f7322665bff91c9b0
Clint R <clinton.ros@gmail.com>
2022-05-07 12:40:09 +0930

    Add chmod-calc.js for 07-05-2022

---
+++ chmod-calc.js
@@ -0,0 +1,52 @@
+// Lot of junior developer can be stuck when they need to change the access permission to a file or a directory in an Unix-like operating systems.
+
+// To do that they can use the chmod command and with some magic trick they can change the permissionof a file or a directory. For more information about the chmod command you can take a look at the wikipedia page.
+
+// chmod provides two types of syntax that can be used for changing permissions. An absolute form using octal to denote which permissions bits are set e.g: 766. Your goal in this kata is to define the octal you need to use in order to set yout permission correctly.
+
+// Here is the list of the permission you can set with the octal representation of this one.
+
+// User
+// read (4)
+// write (2)
+// execute (1)
+// Group
+// read (4)
+// write (2)
+// execute (1)
+// Other
+// read (4)
+// write (2)
+// execute (1)
+// The method take a hash in argument this one can have a maximum of 3 keys (owner,group,other). Each key will have a 3 chars string to represent the permission, for example the string rw- say that the user want the permission read, write without the execute. If a key is missing set the permission to ---
+
+// Note: chmod allow you to set some special flags too (setuid, setgid, sticky bit) but to keep some simplicity for this kata we will ignore this one.
+
+function chmodCalculator(perm) {
+  let hash = {
+    r: 4,
+    w: 2,
+    x: 1,
+  };
+
+  let props = ["user", "group", "other"];
+  let str = "";
+
+  for (let prop of props) {
+    str += perm.hasOwnProperty(prop) ? calc(perm, prop, hash) : 0;
+  }
+
+  return str;
+}
+
+function calc(perm, prop, hash) {
+  let num = 0;
+  let value = perm[prop];
+  let string = "";
+
+  for (let i = 0; i < value.length; i++) {
+    num += value[i] !== "-" ? hash[value[i]] : 0;
+  }
+  return (string += num);
+}
+
